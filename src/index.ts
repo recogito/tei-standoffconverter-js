b2a1ed116f353f58a8847196740390ee8d2aa18f
Rainer Simon <rainer@rainersimon.io>
2025-05-01 16:32:14 +0200

    Export fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { createStandoffTable } from './standoff';
 import { xml2standoff } from './conversion';
 
-const parseXML = (arg: Element | string) => {
+export const parseXML = (arg: Element | string) => {
   const standoff = xml2standoff(arg);
   return createStandoffTable(standoff);
 }
-
-export default { 
-  parseXML
-};
