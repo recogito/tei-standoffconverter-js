15202ae26496ed914f9d6bb3a3975cf86cccd56c
Rainer Simon <rainer@rainersimon.io>
2025-05-07 13:04:32 +0200

    Added example Node.js converter script

---
+++ test/node-example.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import { parseXML } from '../dist/index.js';
+
+const xml = fs.readFileSync('./test/paradise-lost.xml', 'utf8');
+const parsed = parseXML(xml);
+
+const start = 568;
+const end = 573;
+
+const plaintext = parsed.text();
+console.log(`Creating tag on: '${plaintext.substring(start, end)}'`);
+
+parsed.addStandOff('standoff-2');
+parsed.addStandOffTag('standoff-2', start, end, 'persName');
+
+fs.writeFileSync('./example-result.tei.xml', parsed.xmlString());
+console.log('Done.');
+
